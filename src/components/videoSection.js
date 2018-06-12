fed679019740853ee956a768b3d4fe5f4507d6c2
Zoey Zheng <zoeyyandi@gmail.com>
2018-06-11 22:49:16 -0400

    i give up

--- src/components/videoSection.js
+++ src/components/videoSection.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import videoMp4 from './asset/video.mp4';
 import videoWebm from './asset/video.webm';
 import overlayImg from './asset/RADLOGO.svg';
+import Modal from './modal';
 
 const Wrapper = styled.div`
   position: relative;
@@ -12,16 +13,6 @@ const Wrapper = styled.div`
   padding: 0;
   overflow: hidden;
   background-color: black;
-`;
-const Video = styled.video`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  min-width: 100%;
-  min-height: 100%;
-  z-index: -100;
 `;
 
 const OverLay = styled.div`
@@ -45,14 +36,14 @@ class VideoSection extends Component {
         <video
           style={{
             position: 'absolute',
-            top: 0,
+            top: -400,
             left: 0,
             right: 0,
             bottom: 0,
             minWidth: '100%',
             minHeight: '100%'
           }}
-          autoPlay
+          controls
           muted
           loop
           playsInline
@@ -60,9 +51,9 @@ class VideoSection extends Component {
           <source src={videoMp4} type="video/mp4" />
           <source src={videoWebm} type="video/webm" />
         </video>
-        <OverLay>
+        {/* <OverLay>
           <Img src={overlayImg} />
-        </OverLay>
+        </OverLay> */}
       </Wrapper>
     );
   }
