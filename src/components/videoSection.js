ad4007ef75a1be41f8df136edb2a11426f9d6cce
Zoey Zheng <zoeyyandi@gmail.com>
2018-06-11 23:06:38 -0400

    fuckkkkk

--- src/components/videoSection.js
+++ src/components/videoSection.js
@@ -36,13 +36,14 @@ class VideoSection extends Component {
         <video
           style={{
             position: 'absolute',
-            top: -400,
+            top: 0,
             left: 0,
             right: 0,
             bottom: 0,
             minWidth: '100%',
             minHeight: '100%'
           }}
+          controls
           autoPlay
           muted
           loop
